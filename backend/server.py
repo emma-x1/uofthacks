7ef34596e258e98c138358258124c4b7ee7e1909
Tianxiao Lou <TianxiaoLou@outlook.com>
2025-01-18 17:02:44 -0500

    api updated

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -9,14 +10,38 @@ def StartSession():
     
 @app.route('/current_data', methods=['POST'])
 def CurrentData():
-    data={}
-    
-    return jsonify(data)
+   
+
 
 
 @app.route('/analysis', methods=['POST'])
 def Analysis():
+    data=request.json
+    event=data["event"]
+    temp=[]
+    humidity=[]
+    date=[]
+    time=[]
+
+    with open("data.txt","r") as file:
+        for line in file:
+            parts=line.strip().split(" ")
+            temp.append(parts[0])
+            humidity.append(parts[1])
+            date.append(parts[2])
+            time.append(parts[3])
+            new_session={}
+            new_session["event"]=event
+            new_session["date"]=date
+            new_session["time"]=time
+            new_session["humidity"]=humidity
+            new_session["temperature"]=temp
+
     
+    with open("session.json","r") as file:
+        json_data=json.load(file)
+        json_data["sessions"].append(new_session)
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
