324dda11820e71dae2811ca9cbcf4442c864103a
Tianxiao Lou <TianxiaoLou@outlook.com>
2025-01-18 16:37:04 -0500

    server change

--- backend/server.py
+++ backend/server.py
@@ -2,11 +2,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
-def api():
-    data = request.json
-    print(data)
-    return jsonify(data)
+@app.route('/startSession', methods=['POST'])
+def StartSession():
+    with open("data.txt","w") as file:
+        pass
+
+@app.route('/startSession', methods=['POST'])
+def StartSession():
+
+@app.route('/startSession', methods=['POST'])
+def StartSession():
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
