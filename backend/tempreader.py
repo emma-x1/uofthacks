19c6c52a89c02469957d76823f6a36d8abc091c1
Tianxiao Lou <TianxiaoLou@outlook.com>
2025-01-18 15:18:27 -0500

    add time data

--- backend/tempreader.py
+++ backend/tempreader.py
@@ -1,6 +1,7 @@
 # write a python program that reads the serial stream from the arduino and write that data somewhere
 
 import serial 
+import datetime
 import time
 
 arduino = serial.Serial(port='COM6', baudrate=9600)
@@ -8,7 +9,10 @@ arduino = serial.Serial(port='COM6', baudrate=9600)
 def readTemp():
     data = arduino.readline()
     temp, hum = data.decode().split(' ')
+    currentTime=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f'Temperature: {temp} Humidity: {hum}')
+    with open ("data.txt","w") as file:
+        file.write(f"{temp} {hum} {currentTime}\n")
 
 while True:
     readTemp()
